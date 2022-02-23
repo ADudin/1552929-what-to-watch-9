211fd36dc6c4a8b108c4460ca1d4b0c0c234d7e0
ADudin <gwaeron27@yandex.ru>
2022-02-23 20:56:37 +0600

    Добавил моковые данные для комментария

---
+++ project/src/mocks/comments.ts
@@ -0,0 +1,14 @@
+import {Comment} from '../types/comment';
+
+export const comments: Comment[] = [
+  {
+    id: 1,
+    user: {
+      id: 14,
+      name: 'Corey',
+    },
+    rating: 1.6,
+    comment: 'I personally found this movie to be boring. Definitely one of the most boring movies I\'ve ever seen.',
+    date: '2022-01-22T15:13:26.388Z',
+  },
+];
