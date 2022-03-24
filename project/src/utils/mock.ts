7cbb3626ad40c943d3dcf55b66190836ef13cb0a
ADudin <gwaeron27@yandex.ru>
2022-03-24 23:13:38 +0300

    Написал тесты для редьюсера filmData. Получилось неожиданным поведене теста sendReview

--- project/src/utils/mock.ts
+++ project/src/utils/mock.ts
@@ -1,14 +1,62 @@
 import {
   name,
-  internet
+  internet,
+  date,
+  lorem,
+  image,
+  datatype
 } from 'faker';
 
 import {UserData} from '../types/user-data';
+import {Film} from '../types/film';
+import {Review} from '../types/review';
+
+const getRandomInteger = (): number => {
+  const randomInteger = Math.ceil(Math.random() * 100);
+
+  return randomInteger;
+};
 
 export const makeFakeUserData = (): UserData => ({
-  id: Math.ceil(Math.random() * 100),
+  id: getRandomInteger(),
   email: internet.email(),
   name: name.findName(),
   avatarUrl: internet.avatar(),
   token: internet.password(),
 } as UserData);
+
+export const makeFakeFilmData = (): Film => ({
+  id: getRandomInteger(),
+  name: lorem.word(),
+  posterImage: image.imageUrl(),
+  previewImage: image.imageUrl(),
+  backgroundImage: image.imageUrl(),
+  videoLink: internet.url(),
+  previewVideoLink: internet.url(),
+  description: lorem.text(),
+  rating: getRandomInteger(),
+  scoresCount: getRandomInteger(),
+  director: name.findName(),
+  starring: new Array(4).fill(null).map(() => name.findName()),
+  runTime: getRandomInteger(),
+  genre: lorem.word(),
+  released: date.past().getFullYear(),
+  isFavorite: datatype.boolean(),
+} as Film);
+
+export const makeFakeMessage = (): string => {
+  const message = lorem.sentences();
+
+  return message;
+};
+
+export const makeFakeReview = (): Review => ({
+  comment: lorem.text(),
+  date: date.past().toDateString(),
+  id: getRandomInteger(),
+  rating: getRandomInteger(),
+  user: {
+    id: getRandomInteger(),
+    name: name.findName(),
+  },
+} as Review);
